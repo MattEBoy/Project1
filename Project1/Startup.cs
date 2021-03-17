2e5373a689a38abf96aa67576792df8414a20a51
MattEBoy <teo1997@gmail.com>
2021-03-17 08:39:11 -0600

    added sarah's changes

--- Project1/Startup.cs
+++ Project1/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Project1.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,11 @@ namespace Project1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            object p = services.AddDbContext<AppointmentsContext>(options =>
+            {
+                options.UseSqlite(Configuration["ConnectionStrings:AppointmentsConnection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
